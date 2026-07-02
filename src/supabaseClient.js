8c94a6bd46111ff926c90978d7781d913551cd5f
Davi Nicacio <daviniccacio@gmail.com>
2026-07-02 16:17:49 -0300

    feat: V1 finalizada | acesso com servidor local da empresa

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Substitui com as credenciais que aparecem nas configurações do teu projeto Supabase (Project Settings > API)
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
